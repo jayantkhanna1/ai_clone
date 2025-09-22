e539f42dd1b181d92c74ba7e93568260af99f615
jayantkhanna1 <jayantkhanna3105@gmail.com>
2025-09-22 17:18:35 +0530

    completed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let speechSynthesis = window.speechSynthesis;
 let currentUtterance = null;
 let isPaused = false;
 let currentTheme = 'light';
-let BACKEND_URL = "http://aiclone.jayantkhanna.in";
+let BACKEND_URL = "https://aiclone.jayantkhanna.in";
 
 // Initialize everything when DOM is loaded
 document.addEventListener('DOMContentLoaded', function() {
